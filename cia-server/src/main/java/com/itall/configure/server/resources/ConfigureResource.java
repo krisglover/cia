c272e5dd6ad71b9f5245e1896301910090364990
Kris Glover <kglover@appnexus.com>
2013-10-10 22:21:20 -0400

    basic framework for configuration service

---
+++ cia-server/src/main/java/com/itall/configure/server/resources/ConfigureResource.java
@@ -0,0 +1,62 @@
+package com.itall.configure.server.resources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Path("/configuration")
+public class ConfigureResource {
+
+	private static Logger logger = LoggerFactory.getLogger(ConfigureResource.class);
+	
+	/**
+	 * Returns all default configuration values for the specified environment
+	 * @param env
+	 * @return
+	 */
+	@GET
+	@Path("/{env}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map<String,String> getEnvironmentalConfigs(@PathParam("env") String env){
+	
+		logger.info("Environment : " + env);
+		
+		Map<String,String> configs = new HashMap<>();
+		configs.put("hello", "world");
+		configs.put("key1", "value1");
+		configs.put("key2", "value2");
+		
+		return configs;	
+	}
+	
+	/**
+	 * Returns all configuration values for the specified environment including specific application overrides
+	 * @param env
+	 * @return
+	 */
+	@GET
+	@Path("/{env}/{application}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map<String,String> getApplicationConfigs(@PathParam("env") String env,@PathParam("application") String application){
+	
+		logger.info("Environment : " + env);
+		logger.info("Application : " + application);
+		
+		Map<String,String> configs = new HashMap<>();
+		configs.put("hello", "world");
+		configs.put("key1", "value1");
+		configs.put("key2", "value2");
+		
+		return configs;	
+	}
+	
+}
---
+++ cia-server/src/main/java/com/itall/configure/server/service/CIAConfiguration.java
@@ -0,0 +1,27 @@
+package com.itall.configure.server.service;
+
+import com.yammer.dropwizard.config.Configuration;
+
+/**
+ *
+ * The Configure It All bootstrap configuration object
+ * 
+ * @author kglover
+ *
+ */
+public class CIAConfiguration extends Configuration {
+
+	private String springContextFile;
+
+	public CIAConfiguration(){
+		this.springContextFile = "ciaServerContext.xml";
+	}
+
+	public String getSpringContextFile() {
+		return springContextFile;
+	}
+
+	public void setSpringContextFile(String springContextFile) {
+		this.springContextFile = springContextFile;
+	}
+}
